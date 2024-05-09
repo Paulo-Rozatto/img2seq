49f60218cb82183e7d4f8ea43e4a97663ed50e3b
Paulo Rozatto <rozatto.paulo.victor@gmail.com>
2024-05-08 23:52:51 -0300

    fast drafts of using network to bean leaf dataset

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 A test trying to use ViT to give the segmentation of MNINST digts in the format of annotation polygon.
 
-|![demo](demo.png)|
-|:--:|
 | The idea is something like that but better |
+|:--:|
+|![demo](demo.png)|
+|![demo bean](demo-bean.png)|
--- src/datasets.py
+++ src/datasets.py
@@ -38,4 +38,30 @@ class PolyMNIST(Dataset):
         polygon = polygon.reshape(-1, 3)
         polygon = torch.tensor(polygon, dtype=torch.float)
 
-        return image, label, polygon
+        return image, polygon
+
+
+class PolyBean(Dataset):
+    def __init__(self, csv_file, transform=None, label_transform=None, return_poly=True):
+        self.df = read_csv(csv_file)
+        self.path = path.dirname(csv_file)
+        self.transform = transform
+        self.label_transform = label_transform
+        self.return_poly = return_poly
+
+    def __len__(self):
+        return self.df.shape[0]
+
+    def __getitem__(self, index):
+        image_path = path.join(self.path, self.df.file_path[index])
+        image = Image.open(image_path)
+
+        if self.transform:
+            image = self.transform(image)
+
+        polygon = self.df.polygon[index]
+        polygon = np.fromstring(polygon, sep=",")
+        polygon = polygon.reshape(-1, 3)
+        polygon = torch.tensor(polygon, dtype=torch.float)
+
+        return image, polygon
